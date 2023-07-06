b3f901d9ed4445a85a796aea9c97ed5113bd75f4
GAURAVSINGHVNS <100011519+GAURAVSINGHVNS@users.noreply.github.com>
2023-07-06 12:45:21 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿string firstFriend = "gaurav";
+string secondFriend = "aman";
+Console.WriteLine($"my friends are {firstFriend} and {secondFriend}");
+Console.WriteLine($"the name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"the name {secondFriend} has {secondFriend.Length} letters.");
+Console.ReadKey();
